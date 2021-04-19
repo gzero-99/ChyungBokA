783b18096c05f686b648a3f18b48ddef349d2105
rosach5656@gmail.com <70897603+gzero-99@users.noreply.github.com>
2021-04-20 01:08:06 +0900

    home fragment design 210419

--- Project/app/src/main/java/com/example/chyungboka/HomeFragment.kt
+++ Project/app/src/main/java/com/example/chyungboka/HomeFragment.kt
@@ -1,10 +1,13 @@
 package com.example.chyungboka
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import kotlinx.android.synthetic.main.fragment_home.*
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -21,14 +24,52 @@ class HomeFragment : Fragment() {
     private var param1: String? = null
     private var param2: String? = null
 
+    //viewpager 설정
+    var currentPosition =0
+    //핸들러 설정
+    //ui 변경하기
+    val handler= Handler(Looper.getMainLooper()){
+        setPage()
+        true
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        //setContentView(R.layout.fragment_home)
         arguments?.let {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
         }
+        //어댑터 연결하기
+        val adapter=ViewPagerAdapter()
+        pager.adapter=adapter
+
+        //버튼 클릭시 2page로
+        nextButton.setOnClickListener {
+            currentPosition=2
+            pager.setCurrentItem(2, true)
+        }
+
+        //뷰페이저 넘기는 쓰레드
+        val thread=Thread(PagerRunnable())
+        thread.start()
+    }
+    //페이지 변경하기
+    fun setPage(){
+        if(currentPosition==5) currentPosition=0
+        pager.setCurrentItem(currentPosition,true)
+        currentPosition+=1
     }
 
+    //2초 마다 페이지 넘기기
+    inner class PagerRunnable:Runnable{
+        override fun run() {
+            while(true){
+                Thread.sleep(2000)
+                handler.sendEmptyMessage(0)
+            }
+        }
+    }
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
---
+++ Project/app/src/main/java/com/example/chyungboka/ViewPagerAdapter.kt
@@ -0,0 +1,50 @@
+package com.example.chyungboka
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.viewpager.widget.PagerAdapter
+import kotlinx.android.synthetic.main.banner_page.view.*
+
+class ViewPagerAdapter :PagerAdapter(){
+    private var mContext: Context?=null
+
+    fun ViewPagerAdapter(context: Context){
+        mContext=context;
+    }
+
+    //position에 해당하는 페이지 생성
+    override fun instantiateItem(container: ViewGroup, position: Int): Any {
+        val view=LayoutInflater.from(container.context).inflate(R.layout.banner_page,container,false)
+        //app:srcCompat="@drawable/banner01"
+        //var name = "banner0"+position;
+        //view.banner_image.setImageResource(R.drawable.name);
+        val p :Int = when(position){
+            1->R.drawable.banner01;
+            2->R.drawable.banner02;
+            3->R.drawable.banner03;
+            else->R.drawable.banner04;
+        }
+        view.banner_image.setImageResource(p);
+        container.addView(view)
+        return view
+    }
+
+    //position에 위치한 페이지 제거거
+    override fun destroyItem(container: ViewGroup, position: Int, `object`: Any) {
+        container.removeView(`object` as View?)
+    }
+
+
+    //사용가능한 뷰 개수 리턴
+    override fun getCount(): Int {
+        return 5
+    }
+
+    //페이지뷰가 특정 키 객체(key object)와 연관 되는지 여부
+    override fun isViewFromObject(view: View, `object`: Any): Boolean {
+        return (view==`object`)
+    }
+}
