f446d135e5437490e2e81c9544e5793cce203110
rosach5656@gmail.com <70897603+gzero-99@users.noreply.github.com>
2021-04-20 16:37:46 +0900

    home fragment banner

--- Project/app/src/main/java/com/example/chyungboka/HomeFragment.kt
+++ Project/app/src/main/java/com/example/chyungboka/HomeFragment.kt
@@ -1,5 +1,6 @@
 package com.example.chyungboka
 
+import android.content.Context
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
@@ -8,11 +9,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import kotlinx.android.synthetic.main.fragment_home.*
-
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+import kotlinx.android.synthetic.main.fragment_home.view.*
 
 /**
  * A simple [Fragment] subclass.
@@ -20,12 +17,17 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class HomeFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
     //viewpager 설정
     var currentPosition =0
+    var mainActivity:MainActivity?=null
+    var mContext: Context?=null
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        mainActivity=context as MainActivity
+        mContext=context
+    }
+
     //핸들러 설정
     //ui 변경하기
     val handler= Handler(Looper.getMainLooper()){
@@ -33,27 +35,6 @@ class HomeFragment : Fragment() {
         true
     }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        //setContentView(R.layout.fragment_home)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-        //어댑터 연결하기
-        val adapter=ViewPagerAdapter()
-        pager.adapter=adapter
-
-        //버튼 클릭시 2page로
-        nextButton.setOnClickListener {
-            currentPosition=2
-            pager.setCurrentItem(2, true)
-        }
-
-        //뷰페이저 넘기는 쓰레드
-        val thread=Thread(PagerRunnable())
-        thread.start()
-    }
     //페이지 변경하기
     fun setPage(){
         if(currentPosition==5) currentPosition=0
@@ -75,26 +56,20 @@ class HomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
-    }
+        val view=inflater.inflate(R.layout.fragment_home,container,false)
+        //어댑터 연결하기
+        val adapter=ViewPagerAdapter()
+        view.pager.adapter=adapter
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment HomeFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            HomeFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+        //버튼 클릭시 2page로
+        view.nextButton.setOnClickListener {
+            currentPosition=2
+            view.pager.setCurrentItem(2, true)
+        }
+
+        //뷰페이저 넘기는 쓰레드
+        val thread=Thread(PagerRunnable())
+        thread.start()
+        return view
     }
 }
